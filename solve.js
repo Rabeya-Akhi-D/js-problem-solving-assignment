93be62c9595d06d0a9930f106e77b52f10827010
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 01:59:32 +0600

    Ternary operator

--- solve.js
+++ solve.js
@@ -80,6 +80,20 @@ let largeNumber = Math.max(10,20);
 let squareRoot = Math.sqrt(9);
 
 
+// 10.1. Choose the correct comparison operator to alert true, when x is greater than y. 
+x = 10;
+y = 5;
+
+alert(x>y); // ? 
+
+
+
+// 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+let age = (age < 18) ? "Too young":"Old enough";
+
+
+
+
 
 
 
