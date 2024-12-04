0fe30c01c05622f849d5a41106f8563993616afa
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 02:08:34 +0600

    celcius to fahrenheit

---
+++ solve-2.js
@@ -0,0 +1,9 @@
+// Question sheet-2
+// 1.Write a function to convert Celsius to Fahrenheit. The function should take a single argument, which is the temperature in Celsius.
+function celsiusToFahrenheit(celsius) {
+    return ((celsius*9/5)+32);
+  }
+  
+  console.log(celsiusToFahrenheit(0)); // 32
+  console.log(celsiusToFahrenheit(25)); // 77
+  
