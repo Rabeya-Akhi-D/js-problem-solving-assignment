9eb919a60fcbbed261fc2e13d7bd790b0e77937e
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 01:10:49 +0600

    alert

--- solve.js
+++ solve.js
@@ -40,7 +40,18 @@ const x = {
   alert("Hello World!");
 }
 myFunction();
-// call the function and see the output 
+// call the function and see the output
+
+// 6. Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50").
+const person ={
+    name:"john",
+    age:50
+};
+alert(person.name + "is" + person.age);
+
+
+
 
 
 
