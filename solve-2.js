9d2066ac97642c639939e22bed7273814613ef9b
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 02:37:08 +0600

    math min

--- solve-2.js
+++ solve-2.js
@@ -24,4 +24,11 @@ function sum(a, b) {
   console.log(sum(3, 4)); // 7
   console.log(sum(10, 20)); // 30
   
+//  4.Write a function to find the smallest number in an array. The function should take a single argument, which is the array to search.
+function findSmallestNum(arr) {
+    return Math.min(arr);
+  }
   
+  console.log(findSmallestNum([3, 5, 1, 9])); // 1
+  console.log(findSmallestNum([-1, -5, 0, 10])); // -5
+    
