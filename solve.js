e3d4006a3e5d63973cb13a5b9082b0b8073c486a
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 01:33:33 +0600

    Array related problem

--- solve.js
+++ solve.js
@@ -58,6 +58,18 @@ button.addEventListener("click");
 alert("button clicked");
 
 
+// 7.1. Alert the number of items in an array, using the correct Array property: 
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+// 2. Change the first item of Brand to "Ford".
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+
+    Brand.unshift = "Ford";
+console.log(Brand);
+
 
 
 
