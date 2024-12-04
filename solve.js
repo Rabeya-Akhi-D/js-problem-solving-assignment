c3833a39ebe62d6e11f3314cdb484d6754f7401b
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 00:25:39 +0600

    declare 3 variables

--- solve.js
+++ solve.js
@@ -1,6 +1,22 @@
 // question sheet-1
-// Create a variable called carName, assign the value Volvo to it.
+// 1.Create a variable called carName, assign the value Volvo to it.
 
 let carName = "Volbo" ; 
+
+// 2.On one single line, declare three variables with the following names and values:
+
+firstName = "John"
+lastName = "Doe"
+age = 35
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+{
+    let firstName = "John";  // write variable name
+    let lastName = "Doe" //write variable values 
+    let age = 35 ; // write variable name and values both 
+    
+}
+
 
 
