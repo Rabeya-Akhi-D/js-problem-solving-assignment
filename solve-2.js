74ef2f3ecb5784670656d9aa029618e9c5175642
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 15:22:24 +0600

    Factorial of Number

--- solve-2.js
+++ solve-2.js
@@ -62,3 +62,13 @@ function isArrayEmpty(arr) {
 console.log(isArrayEmpty([])); // true
 console.log(isArrayEmpty([1, 2, 3])); // false
 
+// 8.Write a function to return the factorial of a number. The function should take a single argument, which is the number.
+function factorialize(num) {
+  if (num === 0 || num === 1) return 1;
+  let result = num*factorialize(num-1);
+  return result;
+}
+
+console.log(factorialize(5)); // 120
+console.log(factorialize(7)); // 5040
+
