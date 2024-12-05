1da32bffcaf6c0dfa029ebfbf7fc3c9f55e86304
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 15:37:37 +0600

    function to array

--- solve-2.js
+++ solve-2.js
@@ -99,3 +99,10 @@ console.log(stringLength("hello")); // 5
 console.log(stringLength("world")); // 5
 
 
+// 12.Write a function to merge two arrays. The function should take two arguments, which are the arrays to merge.
+function mergeArrays(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+console.log(mergeArrays([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(mergeArrays(["a", "b"], ["c", "d"])); // ["a", "b", "c", "d"]
