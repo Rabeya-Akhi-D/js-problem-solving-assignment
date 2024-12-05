2dfeafb3d908fdb4faaa5daf4ce8f5dae28bd60e
Rabeya-Akhi <rabeyaakhi2002@gmail.com>
2024-12-05 15:33:38 +0600

    length to a string

--- solve-2.js
+++ solve-2.js
@@ -90,5 +90,12 @@ function toLowerCase(str) {
 console.log(toLowerCase("HELLO WORLD")); // "hello world"
 console.log(toLowerCase("JavaScript")); // "javascript"
 
+// 11.Write a function to find the length of a string. The function should take a single argument, which is the string.
+function stringLength(str) {
+  return str.length;
+}
+
+console.log(stringLength("hello")); // 5
+console.log(stringLength("world")); // 5
 
 
